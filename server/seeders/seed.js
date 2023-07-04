d1522bc82f61a136165c3f4232482718a8b66cd5
zacharydserafin <zacharydserafin@gmail.com>
2023-07-03 21:45:54 -0500

    Set Up Seeders

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,13 +1,13 @@
 const db = require('../config/connection');
 const { Encounter, Enemy, User, Party, PartyMember, Weapon } = require('../models');
-const encounters = require('./encounters.json');
-const enemies = require('./enemies.json');
+// const encounters = require('./encounters.json');
+// const enemies = require('./enemies.json');
 const partyMembers = require('./partyMembers.json');
-const weapons = require('./weapons.json');
+// const weapons = require('./weapons.json');
 
 db.once('open', async () => {
-  await Thought.deleteMany({});
-  await Thought.create(thoughtSeeds);
+  await PartyMember.deleteMany({});
+  await PartyMember.create(partyMembers);
 
   console.log('all done!');
   process.exit(0);
