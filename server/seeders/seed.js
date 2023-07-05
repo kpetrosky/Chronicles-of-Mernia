7443b509f0b2d6f1e997622e28b8cbcabca88d0d
zacharydserafin <zacharydserafin@gmail.com>
2023-07-05 18:19:01 -0500

    Updated Models and Filled Seeds for PMs and Weapons

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -3,9 +3,11 @@ const { Encounter, Enemy, User, Party, PartyMember, Weapon } = require('../model
 // const encounters = require('./encounters.json');
 // const enemies = require('./enemies.json');
 const partyMembers = require('./partyMembers.json');
-// const weapons = require('./weapons.json');
+const weapons = require('./weapons.json');
 
 db.once('open', async () => {
+  await Weapon.deleteMany({});
+  await Weapon.create(weapons);
   await PartyMember.deleteMany({});
   await PartyMember.create(partyMembers);
 
