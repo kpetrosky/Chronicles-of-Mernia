83c2488f81169864b42fac4682051891bf5fc9db
markelldrake99@gmail.com <markelldrake99@icloud.com>
2023-07-05 18:03:24 -0700

    character form

--- client/src/components/CreateParty/index.js
+++ client/src/components/CreateParty/index.js
@@ -1,236 +1,79 @@
+import React, { useState } from 'react';
 
-//want to create an object for each character with  attributes for each object  
+function  ClassForm(){
+let [class, setClass] = useState('');
+
+const ClassType =['Ranger','Melee','Support','Tank']
+
+
+<div>
+      <form className="character-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name your character "
+          value={input}
+          name="text"
+          className="character-input"
+          onChange={handleChange}
+        ></input>
+        <div className="dropdown">
+          <button className={`dropbtn ${class}`}>
+            {class || 'ClassType'}
+          </button>
+          <div className="dropdown-content">
+            <p onClick={() => setClass(ClassType[0])}>Wizard</p>
+            <p onClick={() => setClass(ClassType[0])}>Ranger</p>
+            <p onClick={() => setClass(ClassType[1])}>Rougue</p>
+            <p onClick={() => setClass(ClassType[1])}>Barbarian</p>
+            <p onClick={() => setClass(ClassType[2])}>Cleric</p>
+            <p onClick={() => setClass(ClassType[2])}>Druid</p>
+            <p onClick={() => setClass(ClassType[3])}>Fighter</p>
+            <p onClick={() => setClass(ClassType[3])}>Paladin</p>
+          </div>
+        </div>
+        <button className="party-button">Add Character to party </button>
+        </form>
+   </div>   
+}  
+{/* 
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Character Select
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Ranged</a>
+        <li><a class="dropdown-item" href="#">Wizard</a></li>
+        <li><a class="dropdown-item" href="#">Ranger</a></li>
+    </li>
+    <li><a class="dropdown-item" href="#">Melee</a>
+        <li><a class="dropdown-item" href="#">Barbarian</a></li>
+        <li><a class="dropdown-item" href="#">Rougue</a></li>
+    </li>
+    <li><a class="dropdown-item" href="#">Support</a>
+        <li><a class="dropdown-item" href="#">Cleric</a></li>
+        <li><a class="dropdown-item" href="#">Druid</a></li>
+    
+    </li>
+    <li><a class="dropdown-item" href="#">Tank</a>
+        <li><a class="dropdown-item" href="#">Fighter</a></li>
+        <li><a class="dropdown-item" href="#">Paladin</a></li>
+    </li>
+  </ul>
+</div> */}
+
+{/* 
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setInput('');
+    setClass('');
+  }; */}
+{/* //want to create an object for each character with  attributes for each object  
 
 
 //establish a return function that displays the JSX of the characters 
-
-
-//create an array of objects 
-
-const characters =[
-{
-    class:{
-        type: 'Wizard',
-        category:'Ranged'
-    },
-    weapon:{
-        weaponChoice:'Spellbook'
-
-    },
-    healthPoints:{
-        currentHP:12
-    },
-    speed:{
-        currentSpeed:10
-    },
-    actions:{
-        dodge:{
-            movement:'roll'
-
-        },
-        attack:{
-            base:'Fire Ball ',
-         special: 'Magic Missle',
-        },
-        block:{
-            defend:'Shield'
-        }
-    },
-},
-{
-    class:{
-        type: 'Barbarian',
-        category:'Melee'
-    },
-    weapon:{
-        weaponChoice:'Battleaxe'
-    },
-   
-    healthPoints:{
-        currentHP:15
-    },
-    speed:{
-        currentSpeed:9
-    },
-    actions:{
-        dodge:{
-            movement:'roll'
-        },
-        attack:{
-            base:'Migthy Slash',
-            special:'Battle rage'
-        },
-        block:{
-            defend: 'shield'
-        }
-    },
-},
-{
-    class:{
-        type: 'Rogue',
-        category:'Melee'
-    },
-    weapon:{
-        weaponChoice:'dagger'
-    },
-    healthPoints:{
-        currentHP:13
-    },
-    speed:{
-        currentSpeed:14
-    },
-    actions:{
-        dodge:{
-            movement:'roll'
-        },
-        attack:{
-            base:'Slice',
-            special:'Flash step',
-        },
-        block:{
-            defend:'shield'
-        }
-    },
-},
-{
-    class:{
-        type: 'Ranger',
-        category:'Ranged'
-    },
-    weapon:{
-        weaponChoice:'Longbow'
-    },
- 
-    healthPoints:{
-        currentHP:'10'
-    },
-    speed:{
-        currentSpeed:12
-    },
-    actions:{
-        dodge:{
-            movement:'roll'
-        },
-        attack:{
-            base:'Straight shot',
-            special:'buff potion',
-        },
-        block:{
-            defend:'shield'
-        }
-    },
-},
-{
-    class:{
-        type: 'Cleric',
-        category:'Support'
-    },
-    weapon:{
-        weaponChoice:'Divine Symbol'
-    },
-  
-    healthPoints:{
-        currentHP:10
-    },
-    speed:{
-        currentSpeed:7
-    },
-    actions:{
-        dodge:{
-            movement:'run'
-        },
-        attack:{
-            base:'Divine blessing',
-            special:'Grace from God',
-        },
-        block:{
-            defend:'shield'
-        }
-    },
-},
-{
-    class:{
-        type: 'Druid',
-        category:'Support'
-    },
-    weapon:{
-        weaponChoice:'Staff'
-    },
-  
-    healthPoints:{
-        currentHP:11
-    },
-    speed:{
-        currentSpeed:8
-    },
-    actions:{
-        dodge:{
-            movement:'roll'
-        },
-        attack:{
-            base:'Vine Whip',
-            special:'Mother Nature',
-        },
-        block:{
-            defend:'shield'
-        }
-    },
-},
-{
-    class:{
-        type: 'Paladin',
-        category:'Tank'
-    },
-    weapon:{
-        weaponChoice:'Warhammer'
-    },
-  
-    healthPoints:{
-        currentHP:22
-    },
-    speed:{
-        currentSpeed:5
-    },
-    actions:{
-        dodge:{
-            movement:'side-step'
-        },
-        attack:{
-            base:'Thors Strike',
-            special:'Holy Cloak',
-        },
-        block:{
-            defend:'shield'
-        }
-    },
-},
-{
-    class:{
-        type: 'Fighter',
-        category:'Tank'
-    },
-    weapon:{
-        weaponChoice:'Longsword'
-    },
-
-    healthPoints:{
-        currentHP:21
-    },
-    speed:{
-        currentSpeed:6
-    },
-    actions:{
-        dodge:{
-            movement:'side-step'
-        },
-        attack:{
-            base:'Mighty split',
-            special:'Battle speech',
-        },
-        block:{
-            defend:'shield'
-        }
-    },
-},
-
-]
+//input field for names , option for class, helper funtions to gather attributes for party member 
+//4 seperateforms containing name and a drop down for class, 
+// create an array of objects with each character and map over Array to make 4 seperate models
+// take the models and grab the ID and connect to party that was created 
+// every time an action is taken it creates a global state varriable that creates progression */}
