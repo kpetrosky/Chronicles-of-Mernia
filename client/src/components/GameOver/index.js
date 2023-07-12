e75801f3361dd59c2bee91eac8585a3ee2344acf
kpetrosky <kpetrosky36@gmail.com>
2023-07-11 20:03:05 -0700

    working on the game over fucntion

--- client/src/components/GameOver/index.js
+++ client/src/components/GameOver/index.js
@@ -1,37 +1,38 @@
-import React, { useState } from 'react';
-import './gameOver.css';
+import React from 'react';
 
-export default function GameOver({ handleProgChange }) {
-  const [gameOver, setGameOver] = useState(false);
-  const [win, setWin] = useState(false);
-
-  const handleGameOver = (result) => {
-    setGameOver(true);
-    setWin(result === 'win');
+export default function App() {
+  const handleProgressChange = (progress) => {
+    console.log('Game progress:', progress);
   };
 
   const startNewGame = () => {
-    setGameOver(false);
-    setWin(false);
-    // Additional logic to reset the game can be added here
+    console.log('Starting a new game...');
   };
 
   return (
     <div>
-      {gameOver ? (
-        <div>
-          <h1>{win ? 'You Win!' : 'You Lose!'}</h1>
-          <p>Try again!</p>
-          <button onClick={startNewGame}>Start New Game</button>
-        </div>
-      ) : (
-        <div>
-          <h1>Your Game Content</h1>
-        
-          <button onClick={() => handleGameOver('lose')}>Game Over - You Lose</button>
-          <button onClick={() => handleGameOver('win')}>Game Over - You Win</button>
-        </div>
-      )}
+      <h1>Your Game Content</h1>
+      <button onClick={() => handleProgressChange('lose')}>Game Over - You Lose</button>
+      <button onClick={() => handleProgressChange('win')}>Game Over - You Win</button>
+
+      <GameOver
+        handleProgChange={handleProgressChange}
+        startNewGame={startNewGame}
+      />
+    </div>
+  );
+}
+
+export function GameOver({ handleProgChange, startNewGame }) {
+  const handleGameOver = (result) => {
+    handleProgChange(result);
+  };
+
+  return (
+    <div>
+      <h1>{'Game Over'}</h1>
+      <p>{'Try again!'}</p>
+      <button onClick={startNewGame}>Start New Game</button>
     </div>
   );
 }
